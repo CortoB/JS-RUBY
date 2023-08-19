f9697ed6463d1ad2dec2ab5b8993cf4a90fb0153
unknown <cortobruzda@gmail.com>
2023-08-19 14:09:35 +0200

    Mise au propre du code, et ajout du test Dafalgan

--- javascript/pharmacy.js
+++ javascript/pharmacy.js
@@ -16,58 +16,40 @@ export class Pharmacy {
    * @returns Drugs[]
    */
   updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit -= 1;
-          }
+    for (let i = 0; i < this.drugs.length; i++) {
+      const drug = this.drugs[i];
+
+      if (drug.name === "Magic Pill") {
+        continue;
+      }
+
+      if (drug.name === "Herbal Tea") {
+        drug.benefit = Math.min(
+          50,
+          drug.benefit + (drug.expiresIn <= 0 ? 2 : 1)
+        );
+      } else if (drug.name === "Fervex") {
+        if (drug.expiresIn <= 0) {
+          drug.benefit = 0;
+        } else if (drug.expiresIn <= 5) {
+          drug.benefit = Math.min(50, drug.benefit + 3);
+        } else if (drug.expiresIn <= 10) {
+          drug.benefit = Math.min(50, drug.benefit + 2);
         }
+      } else if (drug.name === "Dafalgan") {
+        drug.benefit = Math.max(
+          0,
+          drug.benefit - (drug.expiresIn <= 0 ? 4 : 2)
+        );
       } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit += 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit += 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit += 1;
-              }
-            }
-          }
-        }
+        drug.benefit = Math.max(
+          0,
+          drug.benefit - (drug.expiresIn <= 0 ? 2 : 1)
+        );
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn -= 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit -= 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit -= this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit += 1;
-          }
-        }
-      }
-      if (this.drugs[i].name === "Dafalgan") {
-        this.drugs[i].benefit -= 2;
-        if (this.drugs[i].benefit < 0) {
-          this.drugs[i].benefit = 0; // Bénéfice jamais négatif
-        }
+
+      if (drug.name !== "Magic Pill") {
+        drug.expiresIn--;
       }
     }
 
--- javascript/pharmacy.test.js
+++ javascript/pharmacy.test.js
@@ -3,10 +3,17 @@ import assert from "node:assert";
 import { Drug, Pharmacy } from "./pharmacy.js";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
+  it("should decrease the benefit and expiresIn for normal drug", () => {
     assert.deepEqual(
       new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue(),
       [new Drug("test", 1, 2)]
+    );
+  });
+
+  it("should decrease the benefit and expiresIn for Dafalgan", () => {
+    assert.deepEqual(
+      new Pharmacy([new Drug("Dafalgan", 2, 5)]).updateBenefitValue(),
+      [new Drug("Dafalgan", 1, 3)]
     );
   });
 });
